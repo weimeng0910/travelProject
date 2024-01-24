55bceceb3a43d57f0704bbe98c4a9f76ef79cca3
JAMES MENG <jamesmeng@JAMESdeMacBook-Air.local>
2024-01-24 12:08:32 +0200

    test:add ErrorBoundary test

--- src/_tests_/ErrorBoundary.test.tsx
+++ src/_tests_/ErrorBoundary.test.tsx
@@ -6,18 +6,23 @@ import { render } from '@testing-library/react';
 import { ErrorBoundary } from '@/common/hooks/errorBoundary';
 
 // A component that throws an error
+//定义一个错误时抛出的组件
 const ErrorThrowingComponent = () => {
   throw new Error('test error');
 };
+
 //A fallback component to render when an error cocurs
+//定义一个正确时渲染的组件
 const FallbackComponent = () => <div>Fallback UI</div>;
+
 describe('ErrorBoundary', () => {
-  //it('renders fallback UI when an error occurs', () => {
-  //  const { getByText } = render(
-  //    <ErrorBoundary fallbackRender={() => <FallbackComponent />}>
-  //      <ErrorThrowingComponent />
-  //    </ErrorBoundary>
-  //  );
-  //  expect(getByText('Fallback UI')).toBeInTheDocument();
-  //});
+  it('renders "Something went wrong." when an error is thrown', () => {
+    const { getByText } = render(
+      <ErrorBoundary fallbackRender={() => <FallbackComponent />}>
+        {/* 使用故意引发错误的ErrorBoundary子组件 ( ) 来渲染该组件。 */}
+        <ErrorThrowingComponent />
+      </ErrorBoundary>
+    );
+    expect(getByText('Fallback UI')).toBeInTheDocument();
+  });
 });
